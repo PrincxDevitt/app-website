d5a3fb237aa6103b547515e7b3c8b08d006eb55c
PrincxDevitt <PrincxDevitt@gmail.com>
2025-01-22 14:24:09 -0500

    Comienzo visual de el navbar y header
    Comence a crear la forma visual del navbar y header de el webside

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Header from './components/Header'
+import './index.css';
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Header/>
+    </div>
+  )
+}
+
+export default App
