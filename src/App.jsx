d31970144e75825b920250ca268d5786c06a3d82
PrincxDevitt <PrincxDevitt@gmail.com>
2025-01-24 17:51:19 -0500

    Se creo el about
    Se dio el estilo para el about

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 
 import Header from './components/Header'
 import './index.css';
+import About from './components/About';
 
 const App = () => {
   return (
     <div className='w-full overflow-hidden'>
-      
       <Header/>
+      <About/>
     </div>
   )
 }
