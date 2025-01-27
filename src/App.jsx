daff06bdf356aca8b1b00ab9c7d931d45d076df5
PrincxDevitt <PrincxDevitt@gmail.com>
2025-01-27 08:07:27 -0500

    Se creo el formulario de contacto
    Se creo el formulario de contacto y el enviar mensaje de gmail

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,18 @@ import './index.css';
 import About from './components/About';
 import Projects from './components/Projects';
 import Testimonial from './components/Testimonial';
+import Contact from './components/Contact';
+import { ToastContainer } from 'react-toastify';
 
 const App = () => {
   return (
     <div className='w-full overflow-hidden'>
+      <ToastContainer/>
       <Header/>
       <About/>
       <Projects/>
       <Testimonial/>
+      <Contact/>
     </div>
   )
 }
