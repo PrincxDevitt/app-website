88e5f0950f256c64ed2c2e4aa203595966d8030a
PrincxDevitt <PrincxDevitt@gmail.com>
2025-01-29 10:20:37 -0500

    Se hizo los movimientos de la pagina
    Proyecto terminado al terminar de hacer los movimientos de la pagina

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { toast } from 'react-toastify';
+import { motion } from 'framer-motion';
 
 const Contact = () => {
 
@@ -30,7 +31,12 @@ const Contact = () => {
     }
   };
   return (
-    <div className='text-center p-6 py-20 lg:px-32 w-full overflow-hidden
+    <motion.div
+      initial={{opacity: 0, x: -200}}
+      transition={{duration: 1}}
+      whileInView={{opacity: 1, x: 0}}
+      viewport={{once:true}}
+    className='text-center p-6 py-20 lg:px-32 w-full overflow-hidden
     ' id='Contact'>
         <h1 className="text-2xl sm:text-4xl font-bold mb-6 text-center">
         Contact{' '}
@@ -64,7 +70,7 @@ const Contact = () => {
         py-2 px-12 mb-10 rounded'>{result ? result : "Send Message"}</button>
       </form>
         
-    </div>
+    </motion.div>
   )
 }
 
